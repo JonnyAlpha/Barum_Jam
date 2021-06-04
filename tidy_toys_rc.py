345f781c66cda5852cfa9bf6de6f756c60a1da9c
JonnyAlpha <billharvs@btinternet.com>
2021-06-04 10:51:46 +0100

    Update tidy_toys_rc.py
    Changed to add single joystick driving

--- tidy_toys_rc.py
+++ tidy_toys_rc.py
@@ -1,9 +1,8 @@
-# Tidy up the Toys - Manual control for driving with grabber control
+# Tidy up the Toys - Manual control for driving using left analogue joystick with grabber control
 # Bill Harvey 28 May 2021
-# Last update 28 May 2021
+# Last update 04 June 2021
 
-# Need to add servo control for grabber
-# Need to change driving to single joystick
+# Need to add servo control for grabber up and down (once lift mech built)
 
 from time import sleep
 from approxeng.input.selectbinder import ControllerResource  # Import Approx Eng Controller libraries
@@ -12,9 +11,7 @@ import UltraBorg3 as UltraBorg
 # import os
 import sys
 
-global launcher
 global TB
-launcher = False
 
 # Setup the ThunderBorg
 TB = ThunderBorg.ThunderBorg()
@@ -51,27 +48,104 @@ UB = UltraBorg.UltraBorg()  # Create a new UltraBorg object
 UB.Init()  # Set the board up (checks the board is connected)
 
 # Set servo to centre
-#UB.SetServoPosition1()
-UB.SetServoPosition4(-0.02)  # Test Servo positioning using ultra_gui.py to obtain start position and insert here
+UB.SetServoPosition1(-1.0)  # Test Servo positioning using ultra_gui.py to obtain start position and insert here
+UB.SetServoPosition2(0) # Test Servo positioning using ultra_gui.py to obtain start position and insert here
+
+def set_speeds(power_left, power_right):
+    TB.SetMotor1(power_left)
+    TB.SetMotor2(power_right)
+
+def stop_motors():
+    TB.MotorsOff()
+
+def mixer(yaw, throttle, max_power=100):
+    """
+    Mix a pair of joystick axes, returning a pair of wheel speeds. This is where the mapping from
+    joystick positions to wheel powers is defined, so any changes to how the robot drives should
+    be made here, everything else is really just plumbing.
+
+    :param yaw: 
+        Yaw axis value, ranges from -1.0 to 1.0
+    :param throttle: 
+        Throttle axis value, ranges from -1.0 to 1.0
+    :param max_power: 
+        Maximum speed that should be returned from the mixer, defaults to 100
+    :return: 
+        A pair of power_left, power_right integer values to send to the motor driver
+    """
+    left = throttle + yaw
+    right = throttle - yaw
+    scale = float(max_power) / max(1, abs(left), abs(right))
+    return int(left * scale), int(right * scale)
 
 def main():
-    print("started main")
+    print("Program controller loop started")
     while True:
         try:
             try:
-                print("started try joystick")
+                print("Use left and right joysticks to drive")
+                print("Use Controller Square and Controller Circle to open / close grabber")
+                print("Use Controller Triangle and Controller Cross to Lower / Lift grabber")
                 with ControllerResource() as joystick:
                     print("Found a joystick and connected")
+                    print(joystick.controls)
+                    # Loop until joystick disconnects 
                     while joystick.connected:
-                        left_y = joystick["ly"]
+                        # Get joystick values from the left analogue stick
+                        x_axis, y_axis = joystick['lx', 'ly']
+                        # Get power from mixer function
+                        power_left, power_right = mixer(yaw=x_axis, throttle=y_axis)
+                        # Set motor speeds
+                        set_speeds(power_left, power_right)
+                        
+                        # Get joystick values from the left and right joysticks
+                        #left_y = joystick["ly"]
                         # print("Left Joy")
-                        right_y = joystick["ry"]
+                        #right_y = joystick["ry"]
                         # print("Right Joy")
-                        driveLeft = left_y
-                        driveRight = right_y
+                        #driveLeft = left_y
+                        #driveRight = right_y
 
-                        TB.SetMotor1(driveRight)
-                        TB.SetMotor2(driveLeft)
+                        #TB.SetMotor1(driveRight)
+                        #TB.SetMotor2(driveLeft)
+                        
+                        # Get a ButtonPresses object containing everything that was pressed since the last iteration of the loop
+                        joystick.check_presses()
+                        # Print any buttons that were pressed
+                        if joystick.has_presses:
+                            print(joystick.presses)
+
+                        # Check for button presses since the last loop
+                        presses = joystick.check_presses()
+                        servo1 = 0
+                        servo2 = 0
+                        
+                        if joystick.presses.square:
+                            print("Square Pressed")
+                            servo1 = -1.0
+                            UB.SetServoPosition1(servo1)
+
+                        if joystick.presses.circle:
+                            print("Circle Pressed")
+                            servo1 = -0.23
+                            UB.SetServoPosition1(servo1)
+
+                        if joystick.presses.triangle:
+                            print("Triangle Pressed")
+                            servo2 = -1.0
+                            UB.SetServoPosition2(servo2)
+
+                        if joystick.presses.triangle:
+                            print("Cross Pressed")
+                            servo2 = -0.23
+                            UB.SetServoPosition2(servo2)
+
+                        if joystick.has_presses:
+                            print(joystick.presses)
+                            servo1_pos = UB.GetServoPosition1()
+                            print("servo 1 = ", servo1_pos)
+                            servo2_pos = UB.GetServoPosition2()
+                            print("servo 2 = ", servo2_pos)
 
                 # Joystick disconnected.....
                 print("Connection to joystick lost")
@@ -92,4 +166,3 @@ def main():
 
 
 main()
-
